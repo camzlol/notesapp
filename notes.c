e7878ea6405ae0a29b5fad67c5c8a963774ab0c5
camzlol <camzkrr@gmail.com>
2025-03-27 21:51:13 +0000

    added fgets to enter number input

--- notes.c
+++ notes.c
@@ -4,50 +4,51 @@
 #include <unistd.h>
 
 int main(void) {
-    int menu_input;
+    char input_buffer[100];
     char nano[100];
     char note_name[100];
     printf("NEW NOTE: 1\n");
     printf("OPEN NOTE: 2\n");
     printf("DELETE NOTE: 3\n");
     printf("Enter A Number: ");
-    scanf("%d", &menu_input);
-    while (getchar() != '\n');
-    switch (menu_input) {
-        case 1:
-            printf("What do you want to call the note: \n");
-            if (fgets(note_name, 100, stdin) != NULL) {
-                note_name[strcspn(note_name, "\n")] = '\0';
-            }
-            sprintf(nano, "nano %s", note_name);
-            system(nano);
-            break;
-        case 2:
-            printf("What note would you like to open: ");
-            if (fgets(note_name, 100, stdin) != NULL) {
-                note_name[strcspn(note_name, "\n")] = '\0';
-            }
-            if (access(note_name, F_OK) != 0) {
-                printf("File does not exist!\n");
-            } else {
-                snprintf(nano, sizeof(nano), "nano %s", note_name);
+    if (fgets(input_buffer, sizeof(input_buffer), stdin) != NULL) {
+        int menu_input = atoi(input_buffer);
+        switch (menu_input) {
+            case 1:
+                printf("What do you want to call the note: ");
+                if (fgets(note_name, 100, stdin) != NULL) {
+                    note_name[strcspn(note_name, "\n")] = '\0';
+                }
+                sprintf(nano, "nano %s", note_name);
                 system(nano);
-            }
-            break;
-        case 3:
-            printf("What note would you like to delete: ");
-            if (fgets(note_name, 100, stdin) != NULL) {
-                note_name[strcspn(note_name, "\n")] = '\0';
-            }
-            if (access(note_name, F_OK) != 0) {
-                printf("This file does not exist!\n"); 
-            } else {
-                remove(note_name);
-            }
-            break;
-        default:
-            return EXIT_FAILURE;
-            break;
+                break;
+            case 2:
+                printf("What note would you like to open: ");
+                if (fgets(note_name, 100, stdin) != NULL) {
+                    note_name[strcspn(note_name, "\n")] = '\0';
+                }
+                if (access(note_name, F_OK) != 0) {
+                    printf("File does not exist!\n");
+                } else {
+                    snprintf(nano, sizeof(nano), "nano %s", note_name);
+                    system(nano);
+                }
+                break;
+            case 3:
+                printf("What note would you like to delete: ");
+                if (fgets(note_name, 100, stdin) != NULL) {
+                    note_name[strcspn(note_name, "\n")] = '\0';
+                }
+                if (access(note_name, F_OK) != 0) {
+                    printf("This file does not exist!\n"); 
+                } else {
+                    remove(note_name);
+                }
+                break;
+            default:
+                return EXIT_FAILURE;
+                break;
+        }
     }
     return EXIT_SUCCESS;
 }
